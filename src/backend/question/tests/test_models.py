b856208c271b802dea2de637e722873b8a022496
Mive <luoyulin9103@gmail.com>
2023-08-06 17:58:15 -0700

    remove tests.py

--- src/backend/question/tests/test_models.py
+++ src/backend/question/tests/test_models.py
@@ -1,95 +1,95 @@
 from django.test import TestCase, override_settings
 from django.utils import timezone
-from question.models import Category, Question, Ability
+from question.models import Category, Question, Ability, Solution
 
 
-# class CategoryModelTest(TestCase):
+class CategoryModelTest(TestCase):
 
-#     def setUp(self):
-#         self.category = Category.objects.create(name='Test Category', weight=2)
+    def setUp(self):
+        self.category = Category.objects.create(name='Test Category', weight=2)
 
-#     def test_category_creation(self):
-#         """
-#         Test if the Category model can be created correctly.
-#         """
-#         self.assertEqual(self.category.name, 'Test Category')
-#         self.assertEqual(self.category.weight, 2)
-#         self.assertLessEqual(self.category.created_at, timezone.now())
-#         self.assertLessEqual(self.category.updated_at, timezone.now())
-#         self.assertEqual(str(self.category), 'Test Category')
+    def test_category_creation(self):
+        """
+        Test if the Category model can be created correctly.
+        """
+        self.assertEqual(self.category.name, 'Test Category')
+        self.assertEqual(self.category.weight, 2)
+        self.assertLessEqual(self.category.created_at, timezone.now())
+        self.assertLessEqual(self.category.updated_at, timezone.now())
+        self.assertEqual(str(self.category), 'Test Category')
 
-#     def test_default_weight_value(self):
-#         """
-#         Test if the default weight value is correctly set when not provided.
-#         """
-#         default_category = Category.objects.create(name='Default Category')
-#         self.assertEqual(default_category.weight, 1)
+    def test_default_weight_value(self):
+        """
+        Test if the default weight value is correctly set when not provided.
+        """
+        default_category = Category.objects.create(name='Default Category')
+        self.assertEqual(default_category.weight, 1)
 
-#     def test_category_name_max_length(self):
-#         """
-#         Test if the name field has the correct max length constraint.
-#         """
-#         max_length = Category._meta.get_field('name').max_length
-#         self.assertEqual(max_length, 30)
+    def test_category_name_max_length(self):
+        """
+        Test if the name field has the correct max length constraint.
+        """
+        max_length = Category._meta.get_field('name').max_length
+        self.assertEqual(max_length, 30)
 
 
-# class QuestionModelTest(TestCase):
+class QuestionModelTest(TestCase):
 
-#     def setUp(self):
-#         self.category = Category.objects.create(name='Test Category', weight=2)
-#         self.question = Question.objects.create(
-#             name='Test Question',
-#             description={'content': 'This is a test question.'},
-#             upvote=10,
-#             downvote=3,
-#             publish=True,
-#             URL='http://example.com/test',
-#         )
-#         self.question.category_id_list.add(self.category)
+    def setUp(self):
+        self.category = Category.objects.create(name='Test Category', weight=2)
+        self.question = Question.objects.create(
+            name='Test Question',
+            description={'content': 'This is a test question.'},
+            upvote=10,
+            downvote=3,
+            publish=True,
+            URL='http://example.com/test',
+        )
+        self.question.category_id_list.add(self.category)
 
-#     def test_question_creation(self):
-#         """
-#         Test if the Question model can be created correctly.
-#         """
-#         self.assertEqual(self.question.name, 'Test Question')
-#         self.assertEqual(self.question.description, {'content': 'This is a test question.'})
-#         self.assertEqual(self.question.upvote, 10)
-#         self.assertEqual(self.question.downvote, 3)
-#         self.assertTrue(self.question.publish)
-#         self.assertEqual(self.question.URL, 'http://example.com/test')
-#         self.assertLessEqual(self.question.created_at, timezone.now())
-#         self.assertLessEqual(self.question.updated_at, timezone.now())
-#         self.assertEqual(str(self.question), 'Test Question')
+    def test_question_creation(self):
+        """
+        Test if the Question model can be created correctly.
+        """
+        self.assertEqual(self.question.name, 'Test Question')
+        self.assertEqual(self.question.description, {'content': 'This is a test question.'})
+        self.assertEqual(self.question.upvote, 10)
+        self.assertEqual(self.question.downvote, 3)
+        self.assertTrue(self.question.publish)
+        self.assertEqual(self.question.URL, 'http://example.com/test')
+        self.assertLessEqual(self.question.created_at, timezone.now())
+        self.assertLessEqual(self.question.updated_at, timezone.now())
+        self.assertEqual(str(self.question), 'Test Question')
 
-#     def test_question_category_relationship(self):
-#         """
-#         Test if the question is associated with the correct category.
-#         """
-#         self.assertEqual(self.question.category_id_list.count(), 1)
-#         self.assertEqual(self.question.category_id_list.first(), self.category)
+    def test_question_category_relationship(self):
+        """
+        Test if the question is associated with the correct category.
+        """
+        self.assertEqual(self.question.category_id_list.count(), 1)
+        self.assertEqual(self.question.category_id_list.first(), self.category)
 
-#     def test_default_upvote_and_downvote_value(self):
-#         """
-#         Test if the default upvote and downvote values are correctly set when not provided.
-#         """
-#         default_question = Question.objects.create(name='Default Question',
-#                                                    description={'content': 'This is a test question.'})
-#         self.assertEqual(default_question.upvote, 1)
-#         self.assertEqual(default_question.downvote, 1)
+    def test_default_upvote_and_downvote_value(self):
+        """
+        Test if the default upvote and downvote values are correctly set when not provided.
+        """
+        default_question = Question.objects.create(name='Default Question',
+                                                   description={'content': 'This is a test question.'})
+        self.assertEqual(default_question.upvote, 1)
+        self.assertEqual(default_question.downvote, 1)
 
-#     def test_question_name_max_length(self):
-#         """
-#         Test if the name field has the correct max length constraint.
-#         """
-#         max_length = Question._meta.get_field('name').max_length
-#         self.assertEqual(max_length, 100)
+    def test_question_name_max_length(self):
+        """
+        Test if the name field has the correct max length constraint.
+        """
+        max_length = Question._meta.get_field('name').max_length
+        self.assertEqual(max_length, 100)
 
-#     def test_question_url_max_length(self):
-#         """
-#         Test if the URL field has the correct max length constraint.
-#         """
-#         max_length = Question._meta.get_field('URL').max_length
-#         self.assertEqual(max_length, 20)
+    def test_question_url_max_length(self):
+        """
+        Test if the URL field has the correct max length constraint.
+        """
+        max_length = Question._meta.get_field('URL').max_length
+        self.assertEqual(max_length, 20)
 
 @override_settings(VALID_ABILITY_KEYS=['binary tree', 'heap', 'linked list'])
 class AbilityModelTest(TestCase):
@@ -194,3 +194,65 @@ class AbilityModelTest(TestCase):
             'linked list': 0,
             'heap': 0,
         })
+
+
+@override_settings(VALID_ABILITY_KEYS=['binary tree', 'heap', 'linked list'])
+class SolutionModelTest(TestCase):
+
+    def setUp(self):
+        # Create a test Category
+        self.category = Category.objects.create(name='Test Category', weight=2)
+
+        # Create a test Question
+        self.question = Question.objects.create(
+            name='Test Question', description={'text': 'This is a test question'}, publish=True)
+
+        # Create a test Ability
+        self.ability = Ability.objects.create(ability={'binary tree': 10, 'heap': 8, 'linked list': 5})
+
+    def test_solution_creation(self):
+        """
+        Test if the Solution model can be created correctly.
+        """
+        solution_data = {
+            'name': 'Test Solution',
+            'question_id': self.question,
+            'ability_id': self.ability,
+            'type': Solution.SolutionType.CODING,
+            'answer': {'text': 'This is a test solution'},
+            'resources': {'link': 'http://test-resource.com'},
+        }
+
+        solution = Solution.objects.create(**solution_data)
+        # Add related categories using set() method
+        solution.category_id_list.set([self.category])
+        self.assertEqual(solution.name, 'Test Solution')
+        self.assertEqual(solution.question_id, self.question)
+        self.assertEqual(list(solution.category_id_list.all()), [self.category])
+        self.assertEqual(solution.ability_id, self.ability)
+        self.assertEqual(solution.type, Solution.SolutionType.CODING)
+        self.assertEqual(solution.answer, {'text': 'This is a test solution'})
+        self.assertEqual(solution.resources, {'link': 'http://test-resource.com'})
+        self.assertIsNotNone(solution.created_at)
+        self.assertIsNotNone(solution.updated_at)
+        self.assertLessEqual(solution.created_at, timezone.now())
+        self.assertLessEqual(solution.updated_at, timezone.now())
+
+    def test_solution_str_method(self):
+        """
+        Test the __str__ method of the Solution model.
+        """
+        solution_data = {
+            'name': 'Test Solution',
+            'question_id': self.question,
+            'type': Solution.SolutionType.CODING,
+            'answer': {'text': 'This is a test solution'},
+            'resources': {'link': 'http://test-resource.com'},
+        }
+
+        solution = Solution.objects.create(**solution_data)
+
+        # Add related categories using set() method
+        solution.category_id_list.set([self.category])
+        expected_str = 'Test Solution - Test Question'
+        self.assertEqual(str(solution), expected_str)
